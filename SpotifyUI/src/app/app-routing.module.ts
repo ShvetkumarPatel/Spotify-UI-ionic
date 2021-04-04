0a0fae6b06193faa7792452a907f15518279031c
ShvetkumarPatel <patelshvet@gmail.com>
2021-04-04 12:10:29 -0400

    Changing the Tabs Bar

--- SpotifyUI/src/app/app-routing.module.ts
+++ SpotifyUI/src/app/app-routing.module.ts
@@ -6,10 +6,6 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  {
-    path: 'album',
-    loadChildren: () => import('./album/album.module').then( m => m.AlbumPageModule)
-  }
 ];
 @NgModule({
   imports: [
