de051b483d7aeecb886906a2aace259559cb49a5
ShvetkumarPatel <patelshvet@gmail.com>
2021-04-04 11:42:43 -0400

    Adding Spotify Stylling

---
+++ SpotifyUI/src/app/directives/shared-directives.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageFadeDirective } from './image-fade.directive';
+
+
+
+@NgModule({
+  declarations: [
+    ImageFadeDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedDirectivesModule { }
