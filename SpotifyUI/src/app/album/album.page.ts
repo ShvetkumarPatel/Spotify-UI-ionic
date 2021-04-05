9464ea9c81970e5b082fc775c19095595dd80253
ShvetkumarPatel <patelshvet@gmail.com>
2021-04-04 20:54:03 -0400

    Album Details Page

--- SpotifyUI/src/app/album/album.page.ts
+++ SpotifyUI/src/app/album/album.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import albums from '../../assets/mockdata//albums';
 
 @Component({
   selector: 'app-album',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.page.scss'],
 })
 export class AlbumPage implements OnInit {
+  data = null;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    const title = this.activatedRoute.snapshot.paramMap.get('title');
+    const decodedTitle = decodeURIComponent(title);
+    this.data = albums[decodedTitle];
   }
 
+    // Helper function for image names
+    dasherize(string) {
+      return string.replace(/[A-Z]/g, function(char, index) {
+        return (index !== 0 ? '-' : '') + char.toLowerCase();
+      });
+    };
 }
